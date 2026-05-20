7baffdd741b998de0e42872bc4fa7233cbcfa89d
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-05-20 01:46:43 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-// Adicionado: Importação das ferramentas do Firestore Database
-import { getFirestore, doc, setDoc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Adicione initializeFirestore aqui:
+import { initializeFirestore, doc, setDoc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDOu9o_Ox7RnVFuHcR8EuJrOfbUrpyO5o",
@@ -13,24 +13,15 @@ const firebaseConfig = {
   appId: "1:578374341571:web:4bb7270c618ee5e9a1a376"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializa os serviços específicos
 const auth = getAuth(app);
-const db = getFirestore(app); // Instância do banco de dados
 
-// Exporta tudo que as telas de cadastro, login e painel vão precisar
+// Inicialização Otimizada
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true
+});
+
 export { 
-  auth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signOut,
-  db,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  increment
+  auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut,
+  db, doc, setDoc, getDoc, updateDoc, increment 
 };
